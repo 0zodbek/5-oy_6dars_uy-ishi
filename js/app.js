27bec22caadb39f466557f5bddeaf0ef3dc1a8a9
0zodbek <ozpdbekov@gmail.com>
2024-06-28 11:58:39 +0500

    K

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
-const vj = document.getElementById('vj')
-const rathod = document.getElementById('rathod')
-const number = document.getElementById('number')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
-const check = document.getElementById('check')
-const sign = document.getElementById('sign')
+const vj = document.getElementById('vj').value;
+const rathod = document.getElementById('rathod').value;
+const number = document.getElementById('number').value;
+const email = document.getElementById('email').value;
+const password = document.getElementById('password');
+const password2 = document.getElementById('confrimassword');
+const check = document.getElementById('check');
+const sign = document.getElementById('sign');
+const Email = document.getElementById('Email');
+const phonenumber = document.getElementById('phonenumber');
+const surname = document.getElementById('surname');
+const name1 = document.getElementById('name1');
+const form = document.querySelector('form');
+const tr2 = document.getElementById('tr2')
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    if(password.value == password2.value) {
+        name1.texContent = vj;
+        surname.texContent = rathod;
+        phonenumber.texContent = number;
+        Email.texContent = email;
+    }
+
+})
