4937ba475f5340e50e97543958f47252fa3cf678
0zodbek <ozpdbekov@gmail.com>
2024-06-28 17:18:54 +0500

    add js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
-const vj = document.getElementById('vj').value;
+const form = document.querySelector('.form');
+
+
+form.addEventListener('submit',function(event){
+    const vj = document.getElementById('vj').value;
 const rathod = document.getElementById('rathod').value;
 const number = document.getElementById('number').value;
 const email = document.getElementById('email').value;
 const password = document.getElementById('password');
-const password2 = document.getElementById('confrimassword');
+const password2 = document.getElementById('confirmpassword');
 const check = document.getElementById('check');
 const sign = document.getElementById('sign');
 const Email = document.getElementById('Email');
 const phonenumber = document.getElementById('phonenumber');
 const surname = document.getElementById('surname');
 const name1 = document.getElementById('name1');
-const form = document.querySelector('form');
 const tr2 = document.getElementById('tr2')
-form.addEventListener('submit',function(event){
+
     event.preventDefault();
     if(password.value == password2.value) {
-        name1.texContent = vj;
-        surname.texContent = rathod;
-        phonenumber.texContent = number;
-        Email.texContent = email;
+        name1.innerHTML = vj;
+        surname.innerHTML = rathod;
+        phonenumber.innerHTML = number;
+        Email.innerHTML = email;
     }
-
 })
